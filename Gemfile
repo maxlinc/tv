185ea0717a4a82cb3c93f5e7f91f47a517d94c29
Max Lincoln <max@devopsy.com>
2013-09-23 21:21:42 -0300

    pacto integration

--- Gemfile
+++ Gemfile
@@ -2,3 +2,8 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in tv.gemspec
 gemspec
+
+gem 'rake'
+gem 'vcr'
+gem 'pry'
+gem 'pacto', :git => 'https://github.com/thoughtworks/pacto.git'
--- lib/tv.rb
+++ lib/tv.rb
@@ -3,6 +3,18 @@ require 'tv/version'
 require 'yaml'
 require 'faraday'
 
+begin
+  require 'pacto'
+  require 'vcr'
+  VCR.configure do |c|
+    c.default_cassette_options = {:record => :once}
+    c.hook_into :webmock
+    c.cassette_library_dir = 'cassettes'
+  end
+rescue LoadError
+  # Will fallback to simple matching
+end
+
 module TV
 
   class << self
@@ -10,6 +22,10 @@ module TV
     include RSpec::Matchers
 
     def play(file)
+      if defined?(VCR)
+        VCR.insert_cassette file
+      end
+
       raise "Could not find #{file}" unless File.exists?(file)
       interactions = YAML.load(File.read(file))['http_interactions']
 
@@ -29,11 +45,27 @@ module TV
           method.headers = request['headers']
         end
 
+      if defined?(Pacto)
+        pacto_match(response, actual)
+      else
         match(response, actual)
+      end
       end
     end
 
     private
+
+    def pacto_match(response, actual)
+      contract_path = 'cassettes/license_contract.json'
+      contract = Pacto.build_from_file(contract_path, 'https://github.com')
+      Pacto.register_contract(contract)
+      Pacto.use(:default)
+      puts "Pacto - validating actual response:"
+      puts contract.validate(actual)
+      puts "Pacto - validating previously recorded response:"
+      obj_response = OpenStruct.new(response)
+      puts contract.validate(obj_response)
+    end
 
     def match(expected, actual)
       expected['headers'].each do |k, v|
